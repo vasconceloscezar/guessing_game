74545b1804b93e7e92b689461c8854d41dd00a21
Cezar Vasconcelos <cvasconcelos@tractian.com>
2022-08-01 02:58:49 -0300

    feat: add prompt to ask for a number and return it's value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Weolcome to - GUESS THE NUMBER -");
+    println!("Please, guess a number between 1 and 100");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
 }
