e76cf861332cf35d12285980209d2d52ab741be3
Cezar Vasconcelos <cvasconcelos@tractian.com>
2022-08-01 03:29:23 -0300

    feat: check if number input matches secret number and loop until user get it right

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Weolcome to - GUESS THE NUMBER -");
-    println!("Please, guess a number between 1 and 100");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {guess}");
+    loop {
+        println!("Please, guess a number between 1 and 100");
 
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
     println!("Secret number is {secret_number}");
 }
