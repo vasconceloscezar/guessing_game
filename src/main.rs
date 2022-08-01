2f7851c6303870110a3e4d0aee0436e853282207
Cezar Vasconcelos <cvasconcelos@tractian.com>
2022-08-01 03:17:24 -0300

    feat: add secret_number randomly generated

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use rand::Rng;
 use std::io;
 fn main() {
     println!("Weolcome to - GUESS THE NUMBER -");
     println!("Please, guess a number between 1 and 100");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {guess}");
+
+    println!("Secret number is {secret_number}");
 }
